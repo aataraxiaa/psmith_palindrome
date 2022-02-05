8e4eff5d1d332d3bcaaa0fb738c8bc9715d6a893
Pete Smith <peadar81@gmail.com>
2022-02-05 09:26:21 +0000

    Refactor to Module, include in String & Integer

--- lib/psmith_palindrome.rb
+++ lib/psmith_palindrome.rb
@@ -2,22 +2,24 @@
 
 require_relative "psmith_palindrome/version"
 
-class String
+module PsmithPalindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?()
     processed_content == processed_content.reverse
   end
 
-  # Returns the letters in the string.
-  def letters
-    chars.select { |char| char.match(/[a-z]/i) }.join
-  end
-
   private
 
     # Returns content for palindrome testing
     def processed_content
-        scan(/[a-z]/i).join.downcase
+        to_s.scan(/[a-z]*\d*/i).join.downcase
     end
+end
+class String
+  include PsmithPalindrome
+end
+
+class Integer
+  include PsmithPalindrome
 end
